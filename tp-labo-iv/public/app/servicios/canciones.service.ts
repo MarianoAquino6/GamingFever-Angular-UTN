05b6603b94f0043d7d50c75232a857c6ecd6aa96
MarianoAquino6 <mariano.aquino@hotmail.com.ar>
2024-10-03 19:49:02 -0300

    Fix sprint 4 Metallica definitivo

--- tp-labo-iv/public/app/servicios/canciones.service.ts
+++ tp-labo-iv/public/app/servicios/canciones.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { concatMap, from, map, of, switchMap } from 'rxjs';
-
+import { concat, concatMap, from, map, switchMap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,9 +8,7 @@ export class CancionesService {
   albums: string[] = ["Kill 'Em All (Remastered)", "Ride The Lightning (Remastered)", "Master Of Puppets (Remastered)", 
                       "...And Justice For All (Remastered)", "Load", "Reload", "Garage Inc.", "St. Anger", "Death Magnetic", 
                       "Metallica (Remastered 2021)" ,"72 Seasons"];
-
   constructor(private http: HttpClient) { }
-
   private getSpotifyToken() {
     const client_id = 'd03a386f1fb548c8b484765b4593f255';
     const client_secret = 'e5db683f5ea24bee868fab7281a69721';
@@ -21,7 +18,6 @@ export class CancionesService {
       Authorization: 'Basic ' + btoa(`${client_id}:${client_secret}`),
       'Content-Type': 'application/x-www-form-urlencoded',
     });
-
     // Obtengo el token de acceso
     return this.http.post<{ access_token: string }>('https://accounts.spotify.com/api/token', body.toString(), { headers });
   }
@@ -30,23 +26,19 @@ export class CancionesService {
     return this.getSpotifyToken().pipe(
       switchMap(tokenResponse => {
         const token = tokenResponse.access_token;
-
         const headers = new HttpHeaders({
           Authorization: `Bearer ${token}`
         });
-
-        const limit = 50; // Limitar a 50 canciones
-        const randomOffset = Math.floor(Math.random() * 1000); // Offset aleatorio para obtener una selección aleatoria de canciones
-        
-        return this.http.get<any>(`https://api.spotify.com/v1/search?q=metallica&type=track&offset=${randomOffset}&limit=${limit}`, { headers }).pipe(
+  
+        // Hacemos una única llamada con el límite de 50 canciones
+        return this.http.get<any>(`https://api.spotify.com/v1/search?q=metallica&type=track&limit=50`, { headers }).pipe(
           map(response => {
-            // Filtrar las canciones para incluir solo las que pertenecen a los álbumes definidos
+            // Filtra las canciones para incluir solo las de los álbumes específicos
             const filteredTracks = response.tracks.items.filter((track: any) => 
               this.albums.includes(track.album.name)
             );
-
-            // Retornar solo 40 canciones o menos si no hay tantas disponibles
-            return filteredTracks.slice(0, 40).map((track: any) => ({
+            // Retorna los temas filtrados
+            return filteredTracks.map((track: any) => ({
               titulo: track.name,
               album: track.album.images[0]?.url, // Imagen del álbum
               audio: track.preview_url // Vista previa del audio
